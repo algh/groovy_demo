1f49f7b8fe6be77e89e6fb91e70372e610acbc6e
Andrew Lee <andrew.lee@smith.co>
2017-12-04 23:01:12 -0500

    removed aws cli wait for demo

--- simpsons-ca/main.tf
+++ simpsons-ca/main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "simpsons_box" {
 
     # add a wait for this to be able to RDP
     # New-Object System.Net.Sockets.TCPClient -ArgumentList "ec2-54-175-237-77.compute-1.amazonaws.com",3389
-    provisioner "local-exec" {
-        command = "aws ec2 wait password-data-available --instance-id ${aws_instance.simpsons_box.id} --region ca-central-1"
-    }
+    # provisioner "local-exec" {
+    #     command = "aws ec2 wait password-data-available --instance-id ${aws_instance.simpsons_box.id} --region ca-central-1"
+    # }
 }
